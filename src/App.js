ac3c78994fdda4cfd6699256c45531ef524412c8
Leen Kim <llcoolk@MACBKPROi7.local>
2018-08-09 19:35:42 -0700

    added header and footer components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./containers/Home";
 import Blog from "./containers/Blog";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
@@ -9,8 +11,10 @@ class App extends Component {
       <div className="App">
         <Router>
           <div>
+            <Header brand="BlogApp" />
             <Route exact path="/" component={Home} />
             <Route path="/blog" component={Blog} />
+            <Footer />
           </div>
         </Router>
       </div>
---
+++ src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <p>&copy; 2018 Leen's Blog, all rights reserved!</p>
+        </div>
+        <div className="col-md-6">
+          <a href="http://facebook.com" target="_blank">
+            facebook
+          </a>
+          <a href="http://instagram.com" target="_blank">
+            instagram
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
