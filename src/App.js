a8a40996c846d2c15760ee8aa4222611f0334262
Leen Kim <llcoolk@MACBKPROi7.local>
2018-08-09 18:56:08 -0700

    added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./containers/Home";
+import Blog from "./containers/Blog";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello world!</h1>
+        <Router>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/blog" component={Blog} />
+          </div>
+        </Router>
       </div>
     );
   }
